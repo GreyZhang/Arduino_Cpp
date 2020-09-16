c696bed97100ee81b301cc4b8694d866c34ed6ca
greyzhang <greyzhang@126.com>
2020-09-16 21:24:52 +0800

    private的使用

---
+++ learning_code/18_private/18_private.ino
@@ -0,0 +1,46 @@
+class Date
+{
+private:
+    int year;
+public:
+    Date::Date(int y, int m, int d);
+    void init(int y, int m, int d);
+private:
+    int month;
+    int day;
+public:
+    void date_display()
+    {
+        Serial.print("year: ");
+        Serial.println(year);
+        Serial.print("month: ");
+        Serial.println(month);
+        Serial.print("day: ");
+        Serial.println(day);
+    }
+};
+
+Date::Date(int y = 0, int m = 0, int d = 0)
+{
+    year = y;
+    month = m;
+    day = d;
+}
+
+void setup()
+{
+    // Open serial communications and wait for port to open:
+    Serial.begin(115200);
+    while (!Serial)
+    {
+        ; // wait for serial port to connect. Needed for native USB port only
+    }
+
+    Date date(2020, 1, 1);
+    date.date_display();
+}
+
+void loop(void)
+{
+}
+
